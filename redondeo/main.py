38755ebaf155f10b936607987b15b68661a5842c
julian-velez <60607869+julian-velez@users.noreply.github.com>
2026-08-14 12:19:48 -0500

    redondeo
    redondeo

---
+++ redondeo/main.py
@@ -0,0 +1,7 @@
+import math  # Importa el módulo 'math', que contiene funciones matemáticas como floor, ceil, sqrt, etc.
+
+print(math.floor(3.7))  # 'math.floor()' redondea hacia abajo el número 3.7, 
+                        # es decir, devuelve el entero más pequeño menor o igual a 3.7 → resultado: 3
+
+print(math.ceil(3.3))   # 'math.ceil()' redondea hacia arriba el número 3.3, 
+                        # es decir, devuelve el entero más grande mayor o igual a 3.3 → resultado: 4
